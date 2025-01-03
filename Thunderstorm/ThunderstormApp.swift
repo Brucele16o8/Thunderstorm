9a393e1e6f62b14f4ec6cb1bdb567867a99c591f
Bruce Le <brucele.tt168@gmail.com>
2025-01-04 01:02:08 +1100

    Initial Commit

---
+++ Thunderstorm/ThunderstormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderstormApp.swift
+//  Thunderstorm
+//
+//  Created by Tung Le on 4/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThunderstormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
